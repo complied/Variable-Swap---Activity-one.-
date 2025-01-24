4b55e978a2110d1e2255bfe4049183d77728cd53
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-01-24 14:11:30 -0800

    My 1st commit

---
+++ README.md
@@ -0,0 +1,5 @@
+First-day class activity
+
+This code swaps two different values that are inputted for the users and outputs them. For example, if the values A = 1 and B = 2 after the program is run, the output for A = 2, B = 1.
+
+
---
+++ main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  Variable Swap cis 25 - Activity one
+//
+//  Created by Subigya Parajuli on 1/24/25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int a, b, temp;
+    
+    cout << "Please enter a number (input for a): ";
+    cin >> a;
+    
+    cout << "Please enter a second number (input for b): ";
+    cin >> b;
+    
+    temp = a;
+    a = b;
+    b = temp;
+    
+    cout << "Your swapped values are: a = " << a << " & b = " << b << endl;
+    
+    return 0;
+}
